fb7a73c63d495ab34de68364e0d9f0e31ef0625d
oli <zhidason@gmail.com>
2023-11-17 17:06:48 +0800

    update dotfiles

--- 02_essential_pkgs.sh
+++ 02_essential_pkgs.sh
@@ -2,9 +2,9 @@
 
 echo "Installing the following essential packages:"
 
-yaypkgs=" apple-fonts google-chrome manjaro-asian-input-support-fcitx5 fcitx5-chinese-addons fcitx5-pinyin-zhwiki fcitx5-pinyin-moegirl flameshot peek pinta dingtalk wps-office-cn wps-office-mui-zh-cn wps-office-fonts ttf-wps-fonts wemeet-bin baidunetdisk-bin sunloginclient scrcpy clash-for-windows-bin"
+yaypkgs=" apple-fonts google-chrome manjaro-asian-input-support-fcitx5 fcitx5-chinese-addons fcitx5-pinyin-zhwiki fcitx5-pinyin-moegirl flameshot peek pinta dingtalk wps-office-cn wps-office-mui-zh-cn wps-office-fonts ttf-wps-fonts wemeet-bin baidunetdisk-bin ttf-font-awesome sunloginclient scrcpy clash-for-windows-bin"
 
-pacmanpkgs="vlc kitty dbeaver tree python-pip neovim neofetch screenkey figlet iputils"
+pacmanpkgs="vlc kitty copyq dbeaver tree python-pip neovim neofetch screenkey figlet iputils ttf-jetbrains-mono-nerd noto-fonts noto-fonts-cjk ttf-hack"
 
 echo $yaypkgs
 
