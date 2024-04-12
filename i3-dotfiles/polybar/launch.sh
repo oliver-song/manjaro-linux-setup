8e59e9f666b498ce22de686f1618966b10364354
oli_s <zhidason@gmail.com>
2024-04-12 15:11:16 +0800

    feat: update conf

--- i3-dotfiles/polybar/launch.sh
+++ i3-dotfiles/polybar/launch.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall polybar
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch polybar
-polybar main -c $(dirname $0)/config.ini &
+# Launch example bar
+polybar -config $HOME/.config/polybar/config.ini main 2>&1 | tee -a /tmp/polybar-main.log & disown
+polybar -config $HOME/.config/polybar/config.ini external 2>&1 | tee -a /tmp/polybar-external.log & disown
