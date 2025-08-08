82387130a09cdd89b64f5f822823b580d4360c67
olis <zhidason@gmail.com>
2025-08-08 11:14:23 +0800

    refactor: update essential packages and i3 configuration for improved input method and window management

--- 02_essential_pkgs.sh
+++ 02_essential_pkgs.sh
@@ -2,7 +2,7 @@
 
 echo "Installing the following essential packages:"
 
-yaypkgs="clipit dida365 wechat-universal-bwrap fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool fcitx5-rime librime apple-fonts google-chrome manjaro-asian-input-support-fcitx5 fcitx5-chinese-addons fcitx5-pinyin-zhwiki fcitx5-pinyin-moegirl flameshot peek pinta dingtalk wps-office-cn wps-office-mui-zh-cn wps-office-fonts ttf-wps-fonts wemeet-bin baidunetdisk-bin ttf-font-awesome sunloginclient scrcpy"
+yaypkgs="clipit dida365 wechat-universal-bwrap ibus-rime librime apple-fonts google-chrome manjaro-asian-input-support-fcitx5 fcitx5-chinese-addons fcitx5-pinyin-zhwiki fcitx5-pinyin-moegirl flameshot peek pinta dingtalk wps-office-cn wps-office-mui-zh-cn wps-office-fonts ttf-wps-fonts wemeet-bin baidunetdisk-bin ttf-font-awesome sunloginclient scrcpy"
 
 pacmanpkgs="picom vlc dbeaver tree python-pip neovim neofetch screenkey figlet iputils ttf-jetbrains-mono-nerd noto-fonts noto-fonts-cjk ttf-hack clash-verge-rev-bin"
 
